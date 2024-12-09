9e92e99971baeaa893cc5a0a31d1e7591e284684
alice-hannah <alice.hannah@gov.scot>
2024-12-09 14:47:22 +0000

    Add fn to check vector length

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+check_length <- function(x,
+                         length = 1,
+                         error_env = rlang::caller_env(),
+                         error_arg = rlang::caller_arg(x)) {
+
+  if (length(x) != length) {
+    cli::cli_abort("{.arg {error_arg}} must be length {length}.")
+  } else {
+    invisible(x)
+  }
+
+}
