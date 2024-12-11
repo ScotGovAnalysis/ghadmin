786af43e8734fbf9cfcfce4d9ac7db121c1d3992
alice-hannah <alice.hannah@gov.scot>
2024-12-11 17:07:02 +0000

    Add code to analyse outide collaborators

---
+++ reviews/2025-01/outside_collaborators.R
@@ -0,0 +1,74 @@
+# Analysis of outside collaborators
+# Date: December 2024
+
+
+# 0 - Load packages ----
+
+library(ghadmin)
+library(dplyr)
+library(here)
+library(readr)
+library(writexl)
+
+
+# 1 - Repository info ----
+
+repos <- repos("scotgovanalysis")
+
+repo_use <-
+  full_join(
+    repo_access("scotgovanalysis", repos$repo),
+    repo_contrib("scotgovanalysis", repos$repo),
+    by = c("repo", "user")
+  ) %>%
+  mutate(contributions = case_when(
+    !is.na(role) & is.na(contributions) ~ 0,
+    TRUE ~ contributions
+  )) %>%
+  arrange(repo)
+
+
+# 2 - Outside collaborators ----
+
+# Who are outside collaborators?
+outside_users <-
+  users("scotgovanalysis", user_type = "outside_collaborators")
+
+# What repositories do they have access to and/or have contributed to?
+outside_repos <-
+  repo_use %>%
+  filter(user %in% outside$user) %>%
+  arrange(tolower(user))
+
+# Who are the admins of repos with outside collaborators?
+outside_admins <-
+  repo_use %>%
+  filter(repo %in% outside_repos$repo &
+           role == "admin") %>%
+  mutate(member_type = case_when(
+    user %in% c("alice-hannah", "tomwilsonsco") ~ "org_admin",
+    user %in% outside$user ~ "outside_collaborator"
+  )) %>%
+  arrange(repo)
+
+
+# 3 - Save data ----
+
+list_data <- list(users  = outside_users,
+                  repos  = outside_repos,
+                  admins = outside_admins)
+
+write_rds(
+  list_data,
+  here("reviews", "2025-01", paste0(Sys.Date(),"outside-collborators.rds"))
+)
+
+# xlsx file for manual editing
+write_xlsx(
+  list_data,
+  here("reviews", "2025-01", paste0(Sys.Date(), "outside-collborators.xlsx")),
+  format_headers = FALSE
+)
+
+
+### END OF SCRIPT ###
