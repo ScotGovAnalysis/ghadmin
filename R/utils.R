0251d580928e672890be75289656bedfbe4ae8d4
alice-hannah <alice.hannah@gov.scot>
2025-02-10 18:30:04 +0000

    Add function to parse md file

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,34 @@ possibly_hoist <- function(..., otherwise = NULL) {
   purrr::possibly(tidyr::hoist, otherwise)(...)
 
 }
+
+
+#' Parse markdown file
+#'
+#' @param md_path Character. File path of markdown file.
+#'
+#' @return Character string of markdown content.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{parse_md(here("confirm-membership.md"))}
+
+parse_md <- function(md_path) {
+
+  if (tools::file_ext(md_path) != "md") {
+    cli::cli_abort(c(
+      "x" = "{.arg md_path} must be a .md file.",
+      "i" = "Supplied {.arg md_path} is a .{tools::file_ext(md_path)} file."
+    ))
+  }
+
+  if (!file.exists(md_path)) {
+    cli::cli_abort(
+      "File does not exist at {.file {md_path}}."
+    )
+  }
+
+  readLines(con = md_path) %>% paste(collapse = "\n")
+
+}
