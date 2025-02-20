9a0349ae5db85ad3a0661926186d41d600ccf745
alice-hannah <alice.hannah@gov.scot>
2025-02-20 17:36:04 +0000

    Develop code to check issues and close completed

--- reviews/2025/03_review-progress.R
+++ reviews/2025/03_review-progress.R
@@ -9,60 +9,49 @@ source(here::here("reviews", "2025", "00_setup.R"))
 
 # 1 - Check status of issues ----
 
+github_issues <- issues(review_params$org, review_params$repo, state = "all")
+
 review_issues <-
-  issues(review_params$org, review_params$repo,
-         state = "all",
-         labels = review_params$label)
+  read_rds(here("reviews", "2025", "data", "2025_review-issues.rds"))
 
-
-# Check issue still exists for all members
-
-opened_issues <-
-  read_rds(here("reviews", "2025", "data", "2025_member-review.rds")) %>%
-  filter(!is.na(issue_number)) %>%
-  select(user, issue_number) %>%
+validate <-
+  review_issues %>%
   left_join(
-    review_issues %>% select(issue_number, state),
+    github_issues %>% select(issue_number, state, body, labels, assignees),
     by = "issue_number"
-  )
-
-
-    by = c("number" = "issue_number")
   ) %>%
+  validate_review_issues(exp_tasks = 3, exp_label = "2025-review")
 
-  # Check issue status valid
-  mutate(
-    task_status = task_status(body),
-    n_tasks = n_tasks(body),
-    check_tasks = case_when(
-      n_tasks == 3 ~ "ok",
-      .default = "investigate"
-    ),
-    check_state = case_when(
-      state == "open" ~ "ok",
-      state == "closed" & task_status == "complete" ~ "ok",
-      .default = "investigate"
-    ),
-    check_assignee = case_when(
-      assignees == user ~ "ok",
-      .default = "investigate"
-    )
-  )
 
-investigate <-
-  summary %>%
-  filter(if_any(starts_with("check_"), ~ . == "investigate"))
+# 2 - Close completed issues ----
 
-if (nrow(investigate) > 0) {
-  cli_abort(
-    "{nrow(investigate)} issues require investigation before proceeding."
-  )
-}
+completed <-
+  github_issues %>%
+  filter(issue_number %in% review_issues$issue_number &
+           task_status(body) == "complete")
+
+closed <-
+  map(
+    completed %>% filter(state == "open") %>% pull(issue_number),
+    \(issue_number) {
+      close_issue(review_params$org, review_params$repo, issue_number)
+    }
+  ) %>%
+  list_rbind()
 
 
 # 3 - Save summary ----
 
+review_issues <-
+  review_issues %>%
+  mutate(
+    complete = if_else(issue_number %in% completed$issue_number, TRUE, complete)
+  )
 
+write_rds(
+  review_issues,
+  here("reviews", "2025", "data", "2025_review-issues.rds")
+)
 
 
 ### END OF SCRIPT ###
