d78270e22b3ae7e942863513dc7c00b073cad274
alice-hannah <alice.hannah@gov.scot>
2025-03-10 10:31:42 +0000

    Post reminder messages and update `review_issues`

--- reviews/2025/04_reminders.R
+++ reviews/2025/04_reminders.R
@@ -7,35 +7,57 @@
 source(here::here("reviews", "2025", "00_setup.R"))
 
 
-# 1 - Get open issues ----
+# 1 - Get review issues ----
 
-to_remind <-
-  issues(review_params$org, review_params$repo,
-         state = "open",
-         labels = review_params$label)
+github_issues <- issues(
+  review_params$org,
+  review_params$repo,
+  state = "open",
+  labels = review_params$label
+)
 
 
 # 2 - Post reminder message ----
 
-reminders <-
+to_remind <- github_issues %>% pull(issue_number)
+
+reminded <-
   map(
-    to_remind$issue_number,
+    to_remind,
     \(issue_number) {
-      review_reminder(
+      review_comment(
         owner = review_params$org,
         repo = review_params$repo,
         issue_number = issue_number,
         body_template = here("reviews", "2025", "templates", "reminder.Rmd"),
-        deadline = review_params$deadline
+        date = review_params$deadline
       )
     }
   ) %>%
   list_rbind()
 
+review_issues <-
+  read_rds(here("reviews", "2025", "data", "2025_review-issues.rds"))
+
+review_issues <- if(nrow(reminded) > 0) {
+  review_issues %>%
+    left_join(reminded %>% select(issue_number, date_commented),
+              by = "issue_number") %>%
+    mutate(reminder_message = if_else(
+      !is.na(date_commented), date_commented, reminder_message
+    )) %>%
+    select(-date_commented)
+} else {
+  review_issues
+}
+
 
 # 3 - Save summary ----
 
-
+write_rds(
+  review_issues,
+  here("reviews", "2025", "data", "2025_review-issues.rds")
+)
 
 
 ### END OF SCRIPT ###
