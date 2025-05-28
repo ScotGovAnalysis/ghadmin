10cf710bdb1683f7caea9134a543ca9634b43bfd
alice-hannah <alice.hannah@gov.scot>
2025-05-28 11:50:23 +0100

    First draft code to remove members

---
+++ reviews/2025/05_remove-members.R
@@ -0,0 +1,73 @@
+# Name: 05_remove-members.R
+# Desc: Remove members and close issues
+# Date: May 2025
+
+# 0 - Run set up ----
+
+source(here::here("reviews", "2025", "00_setup.R"))
+
+
+# 1 - Get list of members to remove ----
+
+review_issues <-
+  read_rds(here("reviews", "2025", "data", "2025_review-issues.rds"))
+
+to_remove <-
+  review_issues %>%
+  filter(is.na(complete))
+
+
+# 2 - Remove members from organisation ----
+
+removed <-
+  map(
+    to_remove$user,
+    \(user) remove_member(review_params$org, user)
+  ) %>%
+  list_rbind()
+
+
+# 3 - Comment and close issues ----
+
+closed <-
+  map(
+    as.numeric(to_remove$issue_number),
+    \(issue_number) {
+      review_comment(
+        review_params$org,
+        review_params$repo,
+        issue_number,
+        body_template = here(
+          "reviews", "2025", "templates", "confirm-removal.Rmd"
+        )
+      )
+      close_issue(review_params$org, review_params$repo, issue_number)
+    }
+  ) %>%
+  list_rbind()
+
+
+# 4 - Save summary ----
+
+review_issues <- if(nrow(removed) > 0 & nrow(closed) > 0) {
+  review_issues %>%
+    left_join(removed %>% select(user, date_removed),
+              by = "user")
+} else {
+  review_issues
+}
+
+review_issues <-
+  review_issues %>%
+  mutate(result = case_when(
+    complete ~ "responded",
+    !is.na(date_removed) ~ "removed"
+  ))
+
+write_rds(
+  review_issues,
+  here("reviews", "2025", "data", "2025_review-issues.rds")
+)
+
+
+### END OF SCRIPT ###
